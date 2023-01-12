95a547dffd6f1b0958bceb6a41528e6ae15fde9a
jnk0629 <deal6789@naver.com>
2023-01-12 17:55:57 +0900

    GUI 4

--- 12202451_전나경.py
+++ 12202451_전나경.py
@@ -1,16 +1,6 @@
-subjects = {
-    '의사소통영어': 'A+',
-    '오래된 미래': 'B+',
-    '양자역학': 'A0'
-}
-student = '전나경'
-subject = '오래된 미래'
-print(student, '학생의 ', subject, '과목 성적은', subjects[subject], '입니다.')
-# old style
-print("%s 학생의 %s 과목 성적은 %s입니다." % (student, subject, subjects[subject]))
-# modern style (format 함수)
-print("{0} 학생의 {1} 과목 성적은 {2}입니다.".format(student, subject, subjects[subject]))
-print("{} 학생의 {} 과목 성적은 {}입니다.".format(student, subject, subjects[subject]))
-# ultra modern style
-# f
-print(f'{student} 학생의 {subject} 과목 성적은 {subjects[subject]}입니다.')
+import tkinter as tk
+
+win = tk.Tk()
+win.geometry('400x300')
+win.title('파이썬 1일차 Preview')
+win.mainloop()
