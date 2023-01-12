9a6527ae9ac32c3414f7e83f60c40c120c9786cc
jnk0629 <deal6789@naver.com>
2023-01-12 17:54:38 +0900

    subjects 3

--- 12202451_전나경.py
+++ 12202451_전나경.py
@@ -1,9 +1,16 @@
-countdown_list = [5, 4, 3, 2, 1, "hey!"]
-# for countdown in 5, 4, 3, 2, 1, "hey!"
-
-for countdown in countdown_list:
-    print(countdown)
-print(countdown_list[3])
-print(countdown_list[-3])
-print(countdown_list[-1])
-print('프로그램 종료')
+subjects = {
+    '의사소통영어': 'A+',
+    '오래된 미래': 'B+',
+    '양자역학': 'A0'
+}
+student = '전나경'
+subject = '오래된 미래'
+print(student, '학생의 ', subject, '과목 성적은', subjects[subject], '입니다.')
+# old style
+print("%s 학생의 %s 과목 성적은 %s입니다." % (student, subject, subjects[subject]))
+# modern style (format 함수)
+print("{0} 학생의 {1} 과목 성적은 {2}입니다.".format(student, subject, subjects[subject]))
+print("{} 학생의 {} 과목 성적은 {}입니다.".format(student, subject, subjects[subject]))
+# ultra modern style
+# f
+print(f'{student} 학생의 {subject} 과목 성적은 {subjects[subject]}입니다.')
